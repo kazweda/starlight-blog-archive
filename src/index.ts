7aab59dcaa9d54a6d71782c3cdb54216ab7a3126
Kazuaki Ueda <kazuaki.ueda@gmail.com>
2026-05-29 15:13:32 +0900

    chore: initial setup

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+export type StarlightBlogArchiveOptions = {
+  /** URL prefix for archive pages. Default: 'archive' */
+  prefix?: string;
+};
+
+export function starlightBlogArchive(
+  _options?: StarlightBlogArchiveOptions,
+) {
+  // TODO: implement
+}
